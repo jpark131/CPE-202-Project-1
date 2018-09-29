d00e99dc508d9b0c659c566c4856dd554c654478
unknown <jake.park2398@gmail.com>
2018-09-29 09:34:39 -0700

    Small update

--- perm_lex.py
+++ perm_lex.py
@@ -1,3 +1,10 @@
-def perm_gen_lex(a): 
+def perm_gen_lex(a):
+    list = []
+    if a == '':
+        return []
+    for char in a:
+        simple = char.replace(char, '')
+        list += [[simple] + perm_gen_lex(simple)]
+    return list
 
- 
+print(perm_gen_lex('abc'))
